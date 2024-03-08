98f9f9166ff5da78ecb69df02f74fc4ca72de172
Yul Moodely <156943280+YulEarnCode@users.noreply.github.com>
2024-03-07 19:38:14 -0500

    Update yul_sql_query.sql

--- yul_sql_query.sql
+++ yul_sql_query.sql
@@ -41,6 +41,45 @@ CREATE TABLE LOCATIONS (
                             DEP_ID_LOC CHAR(9) NOT NULL,
                             PRIMARY KEY (LOCT_ID,DEP_ID_LOC));
 
+
+
 --SELECT ALL COLUMNS FROM A TABLE
 SELECT * FROM TABLE_NAME;
 SELECT * FROM FilmLocations;
+
+--Drop a Table
+DROP TABLE TableName;
+
+--
+SELECT ID, Name FROM COUNTRY ;
+SELECT * FROM COUNTRY WHERE ID <= 5 ;
+SELECT * FROM COUNTRY WHERE CCode = 'CA' ;
+
+
+--SQL Basics
+--SELECT statement is used to fetch data from a database.
+SELECT city FROM placeofinterest;
+--WHERE clause is used to extract only those records that fulfill a specified condition.
+SELECT * FROM placeofinterest WHERE city == 'Rome' ;
+--COUNT is a function that takes the name of a column as argument and counts the number of rows when the column is not NULL.
+SELECT COUNT(country) FROM placeofinterest WHERE country='Canada';
+--DISTINCT function is used to specify that the statement is a query which returns unique values in specified columns.
+SELECT DISTINCT country FROM placeofinterest WHERE type='historical';
+--LIMIT is a clause to specify the maximum number of rows the result set must have.
+SELECT * FROM placeofinterest WHERE airport="pearson" LIMIT 5;
+--INSERT is used to insert new rows in the table.
+INSERT INTO placeofinterest (name,type,city,country,airport) VALUES('Niagara Waterfalls','Nature','Toronto','Canada','Pearson');
+--UPDATE used to update the rows in the table.
+UPDATE placeofinterest SET name = 'Niagara Falls' WHERE name = "Niagara Waterfalls";
+--DELETE statement is used to remove rows from the table which are specified in the WHERE condition.
+DELETE FROM placeofinterest WHERE city IN ('Rome','Vienna');
+
+
+
+
+
+
+
+
+
+
